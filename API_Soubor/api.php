19efc6d3d45dbdac6512be2279ea111fc72b7647
Jan Dostál <u18.410@pslib.cloud>
2021-11-11 22:41:08 +0100

    11. commit

---
+++ API_Soubor/api.php
@@ -0,0 +1,115 @@
+<?php
+
+header('Content-type: application/json');
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Headers: X-API-Key, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Allow-Origin');
+header("Allow: GET, POST");
+header("Access-Control-Max-Age: 3600");
+header('Access-Control-Allow-Methods: GET, POST');
+
+
+try {
+
+	$headers = apache_request_headers();
+    $key = $headers["X-API-Key"];
+
+    if (empty($key)) {
+        throw new Exception("Unauthorized", 401);
+    }
+
+          $value = $core->sql->fetchValue("
+        SELECT `uuid`
+        FROM `sms_account`
+        WHERE `uuid` = '".$core->sql->escape($key)."'
+        ");
+
+
+    if($value == null) {
+        header('WWW-Authenticate: Bearer');
+        throw new Exception("Neplátný nebo chybějící API key v headeru requestu.", 401);
+    }
+   
+
+	$result = array();
+
+	$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $uri = explode( '/', $uri );
+
+
+	if($uri[2] == "api" && $_SERVER['REQUEST_METHOD']=="POST") {
+		//
+		//	prvni case
+		//
+
+        if($_SERVER['CONTENT_TYPE'] != "application/json") {
+            throw new Exception("Neplatný typ obsahu payloadu", 400);
+        }
+
+        $body = file_get_contents("php://input");
+
+        $object = json_decode($body, true);
+ 
+        if (!is_array($object)) {
+            throw new Exception('Špatná struktura payloadu');
+        }
+
+	} else if($uri[2] == "api" && isset($uri[3]) && $uri[4] == "status" && $uri[3] != "" && count($uri) == 5 && $_SERVER['REQUEST_METHOD']=="GET") {
+
+        if (ctype_digit($uri[3]) == false || $uri[3] == "0") {
+            throw new Exception("Špatně zadaný query parametr v URL cestě.", 400);
+        }
+
+        $result = $core->sql->fetchArray("
+            SELECT id, status, sent, created
+            FROM sms_queue 
+            WHERE id = ".(int) $uri[3]."
+            ");
+
+        if ($result == null) {
+            throw new Exception("SMS nebyla nalezena.", 404);
+        }
+	}
+      else if($uri[2] == "api" && $uri[3] == "queue" && $_SERVER['REQUEST_METHOD']=="GET") {
+
+        $result = $core->sql->toArray("
+            SELECT id, body, status, `to` AS `num`, created, sent
+            FROM sms_queue
+            ORDER BY created DESC 
+            ");
+
+    }
+     else if ($uri[2] == "api" && $uri[3] == "stats" && $_SERVER['REQUEST_METHOD']=="GET") {
+
+        $result = $core->sql->toArray("
+            SELECT DATE(sent) AS dateOfSending,
+            COUNT(id) AS numberOfSentSms
+            FROM sms_queue 
+            WHERE status = 'SENT' 
+            AND account = '{$value}'
+            GROUP BY DATE(sent) 
+            HAVING COUNT(id) > 0 
+            ORDER BY DATE(sent) DESC 
+            LIMIT 100
+            ");
+
+
+
+    } else {
+		throw new Exception("Neznama case", 400);
+	}
+
+	http_response_code(200);
+    echo json_encode($result);
+} catch(Exception $e) {
+	$errorCode = $e->getCode();
+	if($errorCode == 200) {
+		$errorCode = 500;
+	}
+	http_response_code($errorCode);
+	echo json_encode(array(
+		"error" => $e->getMessage()
+	));
+}
+exit;
+
+?>
