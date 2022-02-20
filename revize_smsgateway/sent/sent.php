886a40c8f030f71674950e4925133cfe70778d30
Jan Dostál <55654299+JanDostal@users.noreply.github.com>
2022-02-20 15:14:49 +0100

    comments

--- revize_smsgateway/sent/sent.php
+++ revize_smsgateway/sent/sent.php
@@ -1,14 +1,22 @@
 <?php
 
+/*
+Backend část stránky, která vypisuje odeslané SMS
+*/
+
 $startovniSqlOffset = 30;
-$sqlLimit = (int) $startovniSqlOffset;
+$sqlLimit = (int) $startovniSqlOffset; // Nastavení počtu řádků tabulky v jeden okamžik, tedy za jeden SQL dotaz
 
 if(!empty($_GET["p"])) {
-	$sqlLimit .=" OFFSET ".((int)$startovniSqlOffset * (int) $_GET["p"]);
+	$sqlLimit .=" OFFSET ".((int)$startovniSqlOffset * (int) $_GET["p"]); // Zjištění, od kolikaté pozice donačíst další řádky
 }
 
 
 $sqlPodminka = "status = 'SENT'";
+
+/*
+Reakce na formulář ze šablony, dle obsahu formuláře vyfiltruje tabulku
+*/
 if(!empty($_GET["q"])) {
 	$sqlPodminka .= " AND (
 		`to` LIKE '%".$core->sql->escape(str_replace("%", "", $_GET["q"]))."%'
@@ -17,6 +25,7 @@ if(!empty($_GET["q"])) {
 	)";
 }
 
+// Získání odeslaných zpráv
 $odeslaneZpravy = $core->sql->toArray("
 	SELECT sms_queue.*
 	FROM sms_queue
@@ -32,13 +41,18 @@ $ucty = $core->sql->toArray("
 	FROM sms_account
 ", "dual");
 
-
+// Nastavení Smarty systému
 $vystup = new smartyWrapper($core);
+
+// Vybrání uložiště pro šablony
 $vystup->setTemplateDir(__DIR__);
+
+// Přenesení PHP proměnné do šablony
 $vystup->assign("ucty", $ucty);
 
 if($_GET["case"]=="getPage") {
 
+	// Provede při žádosti o donačtění dalších řádků
 	$vystup->display("list_head.tpl");
 	foreach($odeslaneZpravy as $polozka) {
 		$vystup->assign("polozka", $polozka);
@@ -49,7 +63,10 @@ if($_GET["case"]=="getPage") {
 }
 else {
 
+	// Provede se při prvním načtění stránky pouze
 	$vystup->assign("odeslaneZpravy", $odeslaneZpravy);
+	
+	// Výpis obsahu šablony
 	$vystup->display("sent.tpl");
 }
 
